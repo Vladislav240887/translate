d500fd8bf0dd79f1d64232ecd263514e6caead6c
Vlad <vladislavsendzuk567@gmail.com>
2025-04-03 20:07:31 +0200

    first commit

---
+++ lang.js
@@ -0,0 +1,38 @@
+const langArr = {
+    title: {
+        ua: 'Мультимовна сторінка',
+        en: 'Multilingual page',
+        de: 'Mehrsprachige Seite'
+
+    },
+
+    name: {
+        ua: 'Ноутбук Lenovo IdeaPad',
+        en: 'Laptop Lenovo IdeaPad',
+        de: 'Laptop Lenovo IdeaPad'
+    },
+
+    os : {
+        ua: 'ОС',
+        en: 'Operating system',
+        de: 'Betriebssystem'
+    },
+
+    memory : {
+        ua: "ОП",
+        en: 'Memory capacity',
+        de: 'Speicherkapazität'
+    },
+
+    type: {
+        ua: "Тип пам'яті",
+        en: 'Memory type',
+        de: 'Speichertyp'
+    },
+
+    color: {
+        ua: "Колір",
+        en: 'Color',
+        de: 'Farben'
+    },
+}
---
+++ script.js
@@ -0,0 +1,25 @@
+const select = document.getElementById('select')
+const allLang = ['ua', 'en', 'de']
+const changeUrLang = () => {
+  let lang = select.value
+  location.hash = lang // Зміна лише хешу, без перезавантаження сторінки
+  changeLanguage()
+}
+select.addEventListener('change', changeUrLang)
+function changeLanguage() {
+  let hash = window.location.hash.substring(1)
+  if (!allLang.includes(hash)) {
+    location.hash = 'ua'
+    hash = 'ua'
+  }
+  select.value = hash
+  if (!langArr['title'][hash]) return
+  document.querySelector('title').innerHTML = langArr['title'][hash]
+  for (let key in langArr) {
+    let element = document.querySelector(`.lang-${key}`)
+    if (element) {
+      element.innerHTML = langArr[key][hash]
+    }
+  }
+}
+changeLanguage()
